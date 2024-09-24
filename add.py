2afb15a7f8c567eeee1defd21b86681c15c463a4
khafiz12 <karimhafiz7@gmail.com>
2024-09-24 18:55:09 -0400

    Initial commit

---
+++ add.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Add the two numbers and store the result in variable c
+c = a + b
+
+# Print the result
+print(f"The sum of {a} and {b} is: {c}")
